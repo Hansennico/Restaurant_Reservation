5181648479c547f19ab31adb880ff417469c73e8
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2024-01-07 23:44:35 +0700

    Create Menu.java

---
+++ src/main/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+    private String Menuid;
+    private String Menuname;
+    private double price;
+    private String location;
+
+    public Menu(String Menuid, String Menuname, double price, String location) {
+        this.Menuid = Menuid;
+        this.Menuname = Menuname;
+        this.price = price;
+        this.location = location;
+    }
+
+}
