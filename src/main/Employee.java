10574fe7cff46f966d8a25a7b0169254652a3441
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2024-01-07 23:45:29 +0700

    Create Employee.java

---
+++ src/main/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    private String Employeeid;
+    private String Employeename;
+    private String branch;
+
+    public Employee(String Employeeid, String Employeename, String branch) {
+        this.Employeeid = Employeeid;
+        this.Employeename = Employeename;
+        this.branch = branch;
+    }
+
+}
