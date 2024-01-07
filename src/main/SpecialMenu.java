3d1ec8462cadcfed0cb53ba0e881b04d26e94884
gedubrackkk <152798464+gedubrackkk@users.noreply.github.com>
2024-01-07 23:45:02 +0700

    Create SpecialMenu.java

---
+++ src/main/SpecialMenu.java
@@ -0,0 +1,15 @@
+public class SpecialMenu extends Menu {
+    private String story; 
+    public SpecialMenu(String Menuid, String Menuname, double price, String location, String story) {
+        super(Menuid, Menuname, price, location); 
+        this.story = story; 
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+}
